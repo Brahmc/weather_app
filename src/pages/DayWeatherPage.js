d96deee276bb702c6282e5212a5777786a544020
Brahim Carrilho <brahim.carrilho@student.thomasmore.be>
2022-11-13 20:46:20 +0100

    app uses current date

--- src/pages/DayWeatherPage.js
+++ src/pages/DayWeatherPage.js
@@ -1,4 +1,4 @@
-import {Button} from "react-bootstrap";
+import {Button, Container} from "react-bootstrap";
 import {useEffect, useState} from "react";
 import {LocationSearch} from "../components/LocationSearch";
 import {SearchModal} from "../components/SearchModal";
@@ -22,35 +22,36 @@ export function DayWeatherPage() {
                 'x-rapidapi-key': process.env.REACT_APP_METEOSTAT_X_RAPID_API_KEY
             }
         }
-
+        console.log(formatDate(date));
         fetch("https://meteostat.p.rapidapi.com/point/hourly?" + new URLSearchParams({
             lat: location.center[1],
             lon: location.center[0],
-            start: '2020-01-01',
-            end: '2020-01-01',
+            start: formatDate(date),
+            end: formatDate(date),
             alt: '113',
             tz: "Europe/Berlin"
         }), options)
             .then(response => response.json())
             .then(data => {setLocationWeather(data.data); console.log(data)});
 
-    }, [location]);
+    }, [date, location]);
     if(location === undefined) return;
 
     return (
-        <>
+        <Container>
             <Button variant="primary" onClick={() => setShowSearch(!showSearch)}>
                 Search
             </Button>
             <h1>{location.place_name}</h1>
+            <span>{date.toDateString()}</span>
             <SearchModal title='Search' show={showSearch} setShow={setShowSearch}>
                 <LocationSearch />
             </SearchModal>
-        </>
+        </Container>
     );
 }
 
 function formatDate(date) {
-    return date.month + "-" + date.day + "-" + date.year;
+    return date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate();
 }
 
