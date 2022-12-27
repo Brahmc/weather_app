4ef29a16ca8801cf5eebd8e41809548c2195948b
Brahim Carrilho <brahim.carrilho@student.thomasmore.be>
2022-12-27 16:37:47 +0100

    Add column names

--- src/components/DayWeatherData.js
+++ src/components/DayWeatherData.js
@@ -3,19 +3,27 @@ import {WiDirectionUp} from "react-icons/wi";
 
 export function DayWeatherData({weatherData}) {
     return (
-        <div style={{overflow: "hidden"}} className='rounded'>
-            {weatherData?.map(d =>
-                <Row key={d.time} className='p-2'
-                     style={{backgroundImage: `linear-gradient(90deg, rgba(58, ${15.6 * d.wspd} , 204, .8), rgba(0, 0, 0, .024) 17%`,
-                         borderBottom: "solid rgba(255, 255, 255, .4) 2px"}}
-                >
-                    <WeatherDataPoint value={(new Date(d.time).getHours())} width={1} />
-                    <WeatherDataPoint value={<WiDirectionUp style={{transform: `rotate(${d.wdir}deg)`}} />} width={1} />
-                    <WeatherDataPoint value={`${d.wspd} km/h`} note={d.wpgt ? `max ${d.wpgt} km/h` : ''} />
-                    <WeatherDataPoint title='temp' value={d.temp + '°C'} note={`${d.pres} hPa`}/>
-                </Row>
-            )}
-        </div>
+        <>
+            <Row xs={9} style={{paddingLeft: ".6rem"}} >
+                <Col xs={1} style={{padding: 0}}>Hour</Col>
+                <Col xs={1} style={{padding: 0}}>WindDir</Col>
+                <Col xs={5} style={{padding: 0}}>WindSpeed</Col>
+                <Col xs={5} style={{padding: 0}}>Temp</Col>
+            </Row>
+            <div style={{overflow: "hidden"}} className='rounded'>
+                {weatherData?.map(d =>
+                    <Row key={d.time} className='p-2'
+                         style={{backgroundImage: `linear-gradient(90deg, rgba(58, ${15.6 * d.wspd} , 204, .8), rgba(0, 0, 0, .024) 17%`,
+                             borderBottom: "solid rgba(255, 255, 255, .4) 2px"}}
+                    >
+                        <WeatherDataPoint value={(new Date(d.time).getHours())} width={1} />
+                        <WeatherDataPoint value={<WiDirectionUp style={{transform: `rotate(${d.wdir}deg)`}} />} width={1} />
+                        <WeatherDataPoint value={`${d.wspd} km/h`} note={d.wpgt ? `max ${d.wpgt} km/h` : ''} />
+                        <WeatherDataPoint title='temp' value={d.temp + '°C'} note={`${d.pres} hPa`}/>
+                    </Row>
+                )}
+            </div>
+        </>
     )
 }
 
